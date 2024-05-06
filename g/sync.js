43bc458868250b3f0793d510df22efdb8c9fc890
Éric Brison <eric.brison@anakeen.com>
2024-05-06 17:04:49 +0200

    get from  google drive

---
+++ g/sync.js
@@ -0,0 +1,46 @@
+import SongStorage from "../songchordsAPI.js";
+
+/**
+   * Downloads a file
+   * @param{string} realFileId file ID
+   * @return{obj} file status
+   * */
+export async function recordFile(fileName, realFileId) {
+    // Get credentials and build service
+    // TODO (developer) - Use appropriate auth mechanism for your app
+
+
+
+    const fileId = realFileId;
+    try {
+        const file = await gapi.client.drive.files.get({
+            fileId: fileId,
+            alt: 'media',
+        });
+        console.log(file.status);
+        addSong(fileName, file.body);
+        return file.status;
+        
+    } catch (err) {
+        // TODO(developer) - Handle error
+        throw err;
+    }
+}
+
+const songStorage = new SongStorage("songs", "global", "currentSongIndex");
+
+
+function addSong(name, body) {
+
+        // Create new song or update if already recorded
+        let songIndex = songStorage.getSongIndexFromName(name);
+
+        if (!songIndex) {
+            songIndex = songStorage.getNewSongIndex();
+            songStorage.recordSongInStorage("songchordname", name, songIndex);
+        }
+
+        songStorage.recordSongInStorage("songchord", body, songIndex);
+}
+
+window.recordFile = recordFile;
---
+++ songchordsAPI.js
@@ -0,0 +1,139 @@
+
+class SongObject {
+    songchordname;
+    songchord;
+    notation;
+    capo;
+}
+
+export default class SongStorage {
+    constructor(songKey, globalKey, currentKey) {
+        this.storageDataSongKey = songKey;
+        this.storageDataGlobalKey = globalKey;
+        this.storageCurrentIndexKey = currentKey;
+
+    }
+
+    get currentSongIndex() {
+        const value = window.localStorage.getItem(this.storageCurrentIndexKey);
+        return (value === "null" ? null : value)
+    }
+
+    set currentSongIndex(v) {
+        return window.localStorage.setItem(this.storageCurrentIndexKey, v);
+    }
+
+    // -----------------------------------------------------------------
+    // ----------------------------- storage management  ------------
+    // -----------------------------------------------------------------
+
+    recordSongInStorage(key, v, index = null) {
+        const songData = this.getAllSongsStorage();
+        let songIndex = index;
+
+        if (index === null && this.currentSongIndex === null) {
+            this.currentSongIndex = this.getNewSongIndex();
+            window.localStorage.setItem(this.storageCurrentIndexKey, this.currentSongIndex);
+        }
+
+        if (songIndex === null) songIndex = this.currentSongIndex;
+
+        if (!songData[songIndex]) {
+            songData[songIndex] = {};
+        }
+        songData[songIndex][key] = v;
+
+        window.localStorage.setItem(this.storageDataSongKey, JSON.stringify(songData));
+
+    }
+
+    getNewSongIndex() {
+        return Math.random().toString(32).slice(2);
+    }
+
+    getSongIndexFromName(name) {
+        const songs = Object.entries(this.getAllSongsStorage());
+
+        for (const [key, song] of songs) {
+            if (song.songchordname === name) {
+                return key;
+            }
+        }
+        return null;
+    }
+
+    getSongInfoFromStorage(key) {
+        const songData = this.getAllSongsStorage();
+
+        if (!this.currentSongIndex || !songData[this.currentSongIndex]) {
+            return undefined;
+        }
+
+        return songData[this.currentSongIndex][key];
+    }
+
+
+    getGlobalInfoFromStorage(key) {
+        const globData = this.getAllGlobalStorage();
+        return globData[key];
+    }
+
+    setGlobalInfoIntoStorage(key, value) {
+        const globData = this.getAllGlobalStorage();
+        globData[key] = value;
+
+        window.localStorage.setItem(this.storageDataGlobalKey, JSON.stringify(globData));
+    }
+
+
+    deleteCurrentSong() {
+
+        const songData = this.getAllSongsStorage();
+        if (this.currentSongIndex) {
+
+            if (songData[this.currentSongIndex]) {
+                delete songData[this.currentSongIndex];
+
+            }
+
+            const keys = Object.keys(songData);
+
+            if (keys.length > 0) {
+                this.currentSongIndex = keys[0];
+            }
+
+            window.localStorage.setItem(this.storageDataSongKey, JSON.stringify(songData));
+
+        } else {
+            if (songData["null"]) {
+                delete songData["null"];
+                window.localStorage.setItem(this.storageDataSongKey, JSON.stringify(songData));
+            }
+        }
+    }
+
+
+    getAllGlobalStorage() {
+
+        return JSON.parse(window.localStorage.getItem(this.storageDataGlobalKey) || "{}");
+    }
+
+    /**
+     *
+     * @returns {Object.<string,SongObject>}
+     */
+    getAllSongsStorage() {
+        return JSON.parse(window.localStorage.getItem(this.storageDataSongKey) || "{}");
+    }
+
+    replaceNoteInBrackets(line, inHtml) {
+        return line.replaceAll(/\[([A-G][b#]?)([1-9a-zA-Z/]{0,4})\]/g, (s, note, end) => {
+            const tNote = noteTranspose(note).replaceAll(/(ø?)/gu, "");
+            if (inHtml) {
+                return `<span class="inline-chord">${tNote + end}</span>`;
+            } else {
+                return tNote + end;
+            }
+        });
+    }
+}
